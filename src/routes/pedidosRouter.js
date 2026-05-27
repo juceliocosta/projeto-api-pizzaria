9b35a581e424a164646538b70728d210f200b15a
Jucélio José <juceliojdc@gmail.com>
2026-05-27 13:04:24 -0300

    feat: aprimorar controlador de pedidos com validações de usuário e atualizar rotas para incluir autenticação

--- src/routes/pedidosRouter.js
+++ src/routes/pedidosRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const pedidosRoutes = express.Router();
 const pedidos = require('../controllers/pedidosController');
+const { autenticarJWT } = require('../middlewares/autenticacao');
 
-pedidosRoutes.post('/', pedidos.criarPedido);
-pedidosRoutes.get('/', pedidos.obterPedidos);
-pedidosRoutes.put('/:id', pedidos.atualizarPedidoPorID);
-pedidosRoutes.delete('/:id', pedidos.deletarPedidoPorID);
-pedidosRoutes.post('/:id/produtos', pedidos.adicionarProdutoAoPedido);
-pedidosRoutes.get('/:id/produtos', pedidos.listarProdutosDoPedido);
-pedidosRoutes.delete('/:id/produtos/:produtoId', pedidos.removerProdutoDoPedido);
+pedidosRoutes.post('/', autenticarJWT, pedidos.criarPedido);
+pedidosRoutes.get('/', autenticarJWT, pedidos.obterPedidos);
+pedidosRoutes.put('/:id', autenticarJWT, pedidos.atualizarPedidoPorID);
+pedidosRoutes.delete('/:id', autenticarJWT, pedidos.deletarPedidoPorID);
+pedidosRoutes.post('/produtos', autenticarJWT, pedidos.adicionarProdutoAoPedido);
+pedidosRoutes.get('/:id/produtos', autenticarJWT, pedidos.listarProdutosDoPedidoPorID);
+pedidosRoutes.delete('/:id/produtos/:produtoId', autenticarJWT, pedidos.removerProdutoDoPedidoPorID);
 
 module.exports = pedidosRoutes;
