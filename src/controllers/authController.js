1f48bd90e411d01523a800f7b9563ea603e052c1
Jucélio José <juceliojdc@gmail.com>
2026-05-27 08:31:33 -0300

    feat: refatorar controladores de autenticação e usuários, removendo código desnecessário e ajustando a lógica de resposta

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,47 +1,8 @@
-const { Usuario } = require('../models/usuario');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { Usuario } = require('../models');
 
-const registrarController = async (req, res) => {
-try {
-    const { nome, email, senha } = req.body;
-
-    if (!nome || !email || !senha) {
-      return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
-    }
-
-    // Verifica se o e-mail já está cadastrado
-    const usuarioExiste = await Usuario.findOne({ where: { email } });
-    if (usuarioExiste) {
-      return res.status(400).json({ error: 'Este e-mail já está em uso.' });
-    }
-
-    // Criptografa a senha
-    const salt = await bcrypt.genSalt(10);
-    const senhaCriptografada = await bcrypt.hash(senha, salt);
-
-    const novoUsuario = await Usuario.create({
-      nome,
-      email,
-      senha: senhaCriptografada,
-      tipo_usuario: 'cliente'
-    });
-
-    return res.status(201).json({
-      message: 'Usuário cadastrado com sucesso!',
-      usuario: {
-        id: novoUsuario.id,
-        nome: novoUsuario.nome,
-        email: novoUsuario.email
-      }
-    });
-
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Erro ao cadastrar usuário no banco de dados.' });
-  }
-};
-
-const logarController = async (req, res) => {
+const logarUsuario = async (req, res) => {
   try {
     const { email, senha } = req.body;
 
@@ -58,7 +19,7 @@ const logarController = async (req, res) => {
     const payload = {
       id: usuario.id,            
       email: usuario.email,      
-      role: usuario.tipo_usuario
+      role: 'Cliente'
     };
     const token = jwt.sign({ payload }, process.env.SECRET_JWT, { expiresIn: '1h' });
     res.status(200).json({ token , message: 'Logado com sucesso!' });
@@ -80,6 +41,5 @@ const logarController = async (req, res) => {
 };
 
 module.exports = { 
-  registrarController, 
-  logarController 
+  logarUsuario 
 };
