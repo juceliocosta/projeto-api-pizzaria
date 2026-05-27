1e44ca1f1ba1d7e4b0a1a317bcec79cad9a107cc
Jucélio José <juceliojdc@gmail.com>
2026-05-27 10:31:57 -0300

    feat: remover controlador de autenticação e rotas, integrando lógica de login ao controlador de usuários

--- src/routes/authRouter.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../controllers/authController');
-
-router.post('/register', auth.registrarController);
-router.post('/login', auth.logarController);
