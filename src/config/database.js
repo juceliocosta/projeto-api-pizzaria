7d6d3b401aad56858cb604de2c2152a3f54b836e
Jucélio José <juceliojdc@gmail.com>
2026-06-03 19:37:51 -0300

    feat: configurar conexão com o banco de dados  e adicionar dependências necessárias

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,25 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: process.env.DB_DIALECT,
-  storage: "pizzalab.sqlite",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+  }
+);
+
+
+async function testarConexao() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão estabelecida com sucesso.');
+  } catch (error) {
+    console.error('Erro ao conectar ao banco:', error);
+  }
+}
+testarConexao();
 
 module.exports = sequelize;
